f8784dd06438889de48af6a45f46b71cfc073600
Alcee Metevier <almetevier@gmail.com>
2020-05-02 15:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  appendKitten("Joe")
+  parentNode.append("Joe")
   return kittens;
 }
